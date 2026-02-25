e91f913eb32d4579966e15715ff8db98ebea5c40
Ahmed Kalalsh <your.email@example.com>
2026-02-25 22:23:01 +0200

    publish vendors files

---
+++ resources/views/vendor/select-tree/select-tree.blade.php
@@ -0,0 +1,36 @@
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
+    <div
+            wire:ignore
+            x-data
+            x-load-css="[
+        @js(\Filament\Support\Facades\FilamentAsset::getStyleHref('tree', package: 'codewithdennis/filament-select-tree')),
+        @js(\Filament\Support\Facades\FilamentAsset::getStyleHref('custom', package: 'codewithdennis/filament-select-tree'))
+        ]"
+    >
+        <div
+                x-ignore
+                ax-load="visible"
+                ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('tree', package: 'codewithdennis/filament-select-tree') }}"
+                x-data="tree({
+                name: '{{ $getName() }}',
+                state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }},
+                options: {{ json_encode($getTree()) }},
+                searchable: '{{ $isSearchable() }}',
+                showCount: '{{ $getWithCount() }}',
+                placeholder: '{{ $getPlaceholder() }}',
+                disabledBranchNode: '{{ !$getEnableBranchNode() }}',
+                disabled: '{{ $isDisabled() }}',
+                isSingleSelect: '{{ !$getMultiple() }}',
+                isIndependentNodes: '{{ $getIndependent() }}',
+                showTags: '{{ $getMultiple() }}',
+                alwaysOpen: '{{ $getAlwaysOpen() }}',
+                clearable: '{{ $getClearable() }}',
+                emptyText: '{{ $getEmptyLabel() }}',
+                expandSelected: '{{ $getExpandSelected() }}',
+                grouped: '{{ $getGrouped() }}',
+            })"
+        >
+            <div x-ref="tree"></div>
+        </div>
+    </div>
+</x-dynamic-component>
